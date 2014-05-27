d90ef08e61ed45c1e84cf3242ff5196376221def
pyemkey <py.emkey@gmail.com>
2014-05-27 11:16:05 +0200

    Modify generators

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,18 @@ module ManagementSystem
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # The defaula socale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.test_framework :rspec,
+        fxtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end 
   end
 end
